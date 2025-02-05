4e84418b5fdaf36efae6d48436fe249233f92d17
tanjirmalik <tanjirmalik1@gmail.com>
2025-02-05 13:46:22 +0530

    Add files via upload

---
+++ array/one.java
@@ -0,0 +1,37 @@
+public class one {
+    public static void main(String[] args) {
+        int [][] mat1 = {
+            {2,5,8},
+            {8,5,6}
+        };
+        int [][] mat2 = {
+            {2,5,8},
+            {8,5,6}
+        };
+        int [][] result = {
+            {0,0,0},
+            {0,0,0}
+        };
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+                result [i][j] = mat1 [i][j] + mat2 [i][j]; 
+                
+            }
+
+            
+        }
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+            System.out.print(result[i][j]+ " ");
+                result [i][j] = mat1 [i][j] + mat2 [i][j]; 
+                
+            }
+            System.out.println("");
+
+            
+        }
+    }
+}
+    
+
+
